1cdc0d6cb5eb441a1852d3d79fc3358c3bae7319
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-09-17 21:17:43 -0500

    setup css and initial javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,47 +1,34 @@
 
-var questionsList = {
-    eachQuestion: { 
-        0 : "Commonly used datatypes DO NOT include?",
-        1 : "The condition in an if/else statement is enclosed with _______.",
-        2 : "Arrays in JavaScript can be used to store ________.",
-        3 : "String values must be enclosed within ______ when being assigned to variables.",
-        4 : "A very useful tool used during development and debugging for printing content to the debugger is:"
-    }
-};
-
-var choiceList = {
-    eachChoice: { 
-        0 : { //Choice 2 - Answer 3
-            0: "1. Strings",
-            1: "2. Boolean",
-            2: "3. Alerts",
-            3: "4. Numbers"},
-        1 : { //Choice 1 - Answer 2
-            0: "1. Quotes",
-            1: "2. Curly Brackets",
-            2: "3. Parenthesis",
-            3: "4. Square Brackets"},
-        2 : { // Choice 3 - Answer 4
-            0: "1. Numbers and Strings",
-            1: "2. Other Arrays",
-            2: "3. Booleans", 
-            3: "4. All of the Above"},      
-        3 : { //  Choice 2 - Answer 3
-            0: "1. Commas",
-            1: "2. Curly Brackets",
-            2: "3. Quotes", 
-            3: "4. Parentheses"},      
-        4 : { // Choice 3 - Answer 4
-            0: "1. Javascript",
-            1: "2. terminal/bash",
-            2: "3. for loops",
-            3: "console.log"},  
-    }
-};
+var questionsList = [
+  {
+    question:  "Commonly used datatypes DO NOT include?",
+    choices:  ["1. Strings","2. Boolean", "3. Alerts", "4. Numbers"],
+    answer:  "3. Alerts"
+  },
+  {
+    question:  "The condition in an if/else statement is enclosed with _______.",
+    choices:  ["1. Quotes","2. Curly Brackets","3. Parenthesis", "4. Square Brackets"],
+    answer:  "2. Curly Brackets"
+  },
+  {
+    question:  "Arrays in JavaScript can be used to store ________.",
+    choices:  ["1. Numbers and Strings","2. Other Arrays", "3. Booleans", "4. All of the Above"],
+    answer:  "4. All of the Above"
+  },
+  {
+    question:  "String values must be enclosed within ______ when being assigned to variables.",
+    choices:  ["1. Commas", "2. Curly Brackets", "3. Quotes", "4. Parentheses"],
+    answer:  "3. Quotes"
+  },
+  {
+    question:  "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices:  ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log",],
+    answer:  "4. console.log"
+  }
+]
 
 
-
-
+         
 
 
 
