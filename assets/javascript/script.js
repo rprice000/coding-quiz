2bee10fc82c44f7eaf6d63855001a1b8419cd592
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-09-17 19:37:00 -0500

    added javascript questions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -34,14 +34,63 @@ var quizCountDownTime = document.getElementById('timer');
 // Variable to see if user picks right or wrong answer
 var answerDisplay = document.getElementById('answerDisplay');
 // Variable to display questions
-var questionPrompt = document.createElement("questionPrompt");
+var questionPromptEl = document.createElement("questionPrompt");
 // Variable for showing if answered right or wrong
-var scoreDisplay = document.createElement("scoreDisplay");
+var scoreDisplayEl = document.createElement("scoreDisplay");
 // Variable for showing user initials
-var initialsContent = document.createElement("initialsContent");
+var initialsContentEl = document.createElement("initialsContent");
 // Variable for input to place user initials
-var initialsInput = document.createElement("initialsInput");
+var initialsInputEl = document.createElement("initialsInput");
 
+
+// Initial Display of Quiz Content
+choice1Btn.style.display = 'none';
+choice2Btn.style.display = 'none';
+choice3Btn.style.display = 'none';
+choice4Btn.style.display = 'none';
+saveScore.style.display = 'none';
+answerDisplay.style.display='none';
+initialsInput.style.display='none';
+
+var questionsList = {
+    eachQuestion: { 
+        0 : "Commonly used datatypes DO NOT include?",
+        1 : "The condition in an if/else statement is enclosed with _______.",
+        2 : "Arrays in JavaScript can be used to store ________.",
+        3 : "String values must be enclosed within ______ when being assigned to variables.",
+        4 : "A very useful tool used during development and debugging for printing content to the debugger is:"
+    }
+};
+
+var choiceList = {
+    eachChoice: { 
+        0 : { //Choice 2 - Answer 3
+            0: "1. Strings",
+            1: "2. Boolean",
+            2: "3. Alerts",
+            3: "4. Numbers"},
+        1 : { //Choice 1 - Answer 2
+            0: "1. Quotes",
+            1: "2. Curly Brackets",
+            2: "3. Parenthesis",
+            3: "4. Square Brackets"},
+        2 : { // Choice 3 - Answer 4
+            0: "1. Numbers and Strings",
+            1: "2. Other Arrays",
+            2: "3. Booleans", 
+            3: "4. All of the Above"},      
+        3 : { //  Choice 2 - Answer 3
+            0: "1. Commas",
+            1: "2. Curly Brackets",
+            2: "3. Quotes", 
+            3: "4. Parentheses"},      
+        4 : { // Choice 3 - Answer 4
+            0: "1. Javascript",
+            1: "2. terminal/bash",
+            2: "3. for loops",
+            3: "console.log"},  
+    }
+};
 
 
 
